3ac80f2be4f7e3c4e03c8536228eefb1e17f3f1c
pragathibaskar <47776430+pragathibaskar@users.noreply.github.com>
2019-09-25 12:19:33 +0530

    updated we config

--- src/main/java/com/cg/gateway/WebConfiguration.java
+++ src/main/java/com/cg/gateway/WebConfiguration.java
@@ -2,24 +2,14 @@ package com.cg.gateway;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * @author moises.macero
- */
 @Configuration
-@EnableWebMvc
-public class WebConfiguration extends WebMvcConfigurerAdapter {
-
-    /**
-     * Enables Cross-Origin Resource Sharing (CORS)
-     * More info: http://docs.spring.io/spring/docs/current/spring-framework-reference/html/cors.html
-     * @param registry
-     */
-    @Override
-    public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
-
+public class WebConfiguration implements WebMvcConfigurer {
+	public void addCorsMapping(CorsRegistry register) {
+		register
+			.addMapping("/**")
+			.allowedMethods("*")
+			.allowedOrigins("*");
+	}
 }
